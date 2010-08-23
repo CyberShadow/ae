e609b1ec1d932dd68ec61706792a88bf57d2a7e6
Vladimir Panteleev <vladimir@thecybershadow.net>
2010-08-23 12:08:39 +0300

    Finish WindowsConfig

--- os/windows/config.d
+++ os/windows/config.d
@@ -4,6 +4,7 @@ import std.windows.charset;
 import std.contracts;
 import std.traits;
 import std.utf;
+import std.conv;
 
 import win32.windef;
 import win32.winreg;
@@ -23,13 +24,48 @@ static:
 				enforce(bytes % 2 == 0);
 				wchar[] ws = new wchar[bytes / 2];
 				readRaw(name, ws);
-				return to!T(ws);
+				enforce(ws[$-1]==0); // should be null-terminated
+				return to!T(ws[0..$-1]);
+			}
+			else
+			static if (is(T == long) || is(T == ulong))
+			{
+				T value;
+				readRaw(name, (&value)[0..1]);
+				return value;
+			}
+			else
+			static if (is(T : uint) || is(T : bool))
+			{
+				uint value;
+				readRaw(name, (&value)[0..1]);
+				return cast(T)value;
 			}
 			else
 				static assert(0, "Can't read values of type " ~ T.stringof);
 		}
 		catch (Throwable e)
 			return defaultValue;
+	}
+	
+	void write(T)(string name, T value)
+	{
+		static if (is(T : const(char[]))) // strings
+		{
+			wstring ws = to!wstring(value ~ '\0');
+			writeRaw(name, ws, REG_SZ);
+		}
+		else
+		static if (is(T == long) || is(T == ulong))
+			writeRaw(name, (&value)[0..1], REG_QWORD);
+		else
+		static if (is(T : uint) || is(T : bool))
+		{
+			uint dwordValue = cast(uint)value;
+			writeRaw(name, (&dwordValue)[0..1], REG_DWORD);
+		}
+		else
+			static assert(0, "Can't write values of type " ~ T.stringof);
 	}
 	
 private:
@@ -56,6 +92,12 @@ private:
 		DWORD size = dest.length;
 		enforce(RegQueryValueExW(key, toUTF16z(name), null, null, cast(ubyte*)dest.ptr, &size) == ERROR_SUCCESS, "RegQueryValueEx failed");
 		enforce(size == dest.length, "Not enough data read");
+	}
+
+	void writeRaw(string name, const(void)[] dest, DWORD type)
+	{
+		if (!key) openKey();
+		enforce(RegSetValueExW(key, toUTF16z(name), 0, type, cast(ubyte*)dest.ptr, dest.length) == ERROR_SUCCESS, "RegSetValueEx failed");
 	}
 
 	uint getSize(string name)
