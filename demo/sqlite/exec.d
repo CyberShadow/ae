9105bd1a1d811eca4a7ba71759da8a08b167ae58
Vladimir Panteleev <vladimir@thecybershadow.net>
2011-11-21 16:11:55 +0200

    ae.demo.sqlite.exec: Interactive mode

--- demo/sqlite/exec.d
+++ demo/sqlite/exec.d
@@ -45,58 +45,76 @@ import ae.sys.console;
 
 void main(string[] args)
 {
-	if (args.length != 3)
-		return stderr.writeln("Usage: exec DATABASE COMMAND");
-	auto db = new SQLite(args[1]);
-	int idx = 0;
-	string[][] rows;
-	StopWatch sw;
-	sw.start();
-	foreach (cells, columns; db.query(args[2]))
-	{
-		sw.stop();
-		if (rows is null)
-			rows ~= ["#"] ~ array(map!`a.idup`(columns));
-		rows ~= [to!string(idx++)] ~ array(map!`a.idup`(cells));
-		sw.start();
-	}
-	sw.stop();
-	writeln("Query executed in ", dur!"hnsecs"(sw.peek().hnsecs));
-	if (rows.length == 0)
-		return;
-	auto widths = new int[rows[0].length];
-	foreach (row; rows)
-	{
-		assert(row.length == rows[0].length);
-		foreach (i, cell; row)
-			widths[i] = max(widths[i], textWidth(cell));
-	}
-	foreach (j, row; rows)
-	{
-		auto rowLines = array(map!textLines(row));
-		auto lineCount = reduce!max(map!`a.length`(rowLines));
+	if (args.length < 2 || args.length > 3)
+		return stderr.writeln("Usage: exec DATABASE [COMMAND]");
 
-		foreach (line; 0..lineCount)
+	auto db = new SQLite(args[1]);
+
+	void exec(string sql)
+	{
+		int idx = 0;
+		string[][] rows;
+		StopWatch sw;
+		sw.start();
+		foreach (cells, columns; db.query(sql))
 		{
-			foreach (i, lines; rowLines)
-			{
-				if (i) write(" │ ");
-				string col = line < lines.length ? lines[line] : null;
-				write(col, std.array.replicate(" ", widths[i]-col.length));
-			}
-			writeln();
+			sw.stop();
+			if (rows is null)
+				rows ~= ["#"] ~ array(map!`a.idup`(columns));
+			rows ~= [to!string(idx++)] ~ array(map!`a.idup`(cells));
+			sw.start();
 		}
-		if (j==0)
+		sw.stop();
+		writeln("Query executed in ", dur!"hnsecs"(sw.peek().hnsecs));
+		if (rows.length == 0)
+			return;
+		auto widths = new int[rows[0].length];
+		foreach (row; rows)
 		{
-			foreach (i, w; widths)
-			{
-				if (i) write("─┼─");
-				write(std.array.replicate("─", w));
-			}
-			writeln();
+			assert(row.length == rows[0].length);
+			foreach (i, cell; row)
+				widths[i] = max(widths[i], textWidth(cell));
 		}
+		foreach (j, row; rows)
+		{
+			auto rowLines = array(map!textLines(row));
+			auto lineCount = reduce!max(map!`a.length`(rowLines));
+
+			foreach (line; 0..lineCount)
+			{
+				foreach (i, lines; rowLines)
+				{
+					if (i) write(" │ ");
+					string col = line < lines.length ? lines[line] : null;
+					write(col, std.array.replicate(" ", widths[i]-col.length));
+				}
+				writeln();
+			}
+			if (j==0)
+			{
+				foreach (i, w; widths)
+				{
+					if (i) write("─┼─");
+					write(std.array.replicate("─", w));
+				}
+				writeln();
+			}
+		}
+		writeln();
 	}
-	writeln();
+
+	if (args.length == 3)
+		exec(args[2]);
+	else
+		while (!stdin.eof())
+		{
+			write("sqlite> ");
+			stdout.flush();
+			try
+				exec(readln().chomp());
+			catch (Exception e)
+				writeln(e.msg);
+		}
 }
 
 import std.utf;
