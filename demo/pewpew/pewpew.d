845a569298c38f9db1ccf9c27ab0e344341b149c
Vladimir Panteleev <vladimir@thecybershadow.net>
2012-02-14 21:49:17 +0200

    Relicense under MPL 2.0

--- demo/pewpew/pewpew.d
+++ demo/pewpew/pewpew.d
@@ -1,38 +1,16 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 3.0
+/**
+ * Space shooter demo.
  *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
+ * License:
+ *   This Source Code Form is subject to the terms of
+ *   the Mozilla Public License, v. 2.0. If a copy of
+ *   the MPL was not distributed with this file, You
+ *   can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is the ArmageddonEngine library.
- *
- * The Initial Developer of the Original Code is
- * Vladimir Panteleev <vladimir@thecybershadow.net>
- * Portions created by the Initial Developer are Copyright (C) 2011-2012
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of the
- * GNU General Public License Version 3 (the "GPL") or later, in which case
- * the provisions of the GPL are applicable instead of those above. If you
- * wish to allow use of your version of this file only under the terms of the
- * GPL, and not to allow others to use your version of this file under the
- * terms of the MPL, indicate your decision by deleting the provisions above
- * and replace them with the notice and other provisions required by the GPL.
- * If you do not delete the provisions above, a recipient may use your version
- * of this file under the terms of either the MPL or the GPL.
- *
- * ***** END LICENSE BLOCK ***** */
+ * Authors:
+ *   Vladimir Panteleev <vladimir@thecybershadow.net>
+ */
 
-/// Space shooter demo.
 module ae.demo.pewpew.pewpew;
 
 import std.random;
--- net/irc/common.d
+++ net/irc/common.d
@@ -1,41 +1,19 @@
-﻿/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 3.0
+﻿/**
+ * Common IRC code.
  *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
+ * License:
+ *   This Source Code Form is subject to the terms of
+ *   the Mozilla Public License, v. 2.0. If a copy of
+ *   the MPL was not distributed with this file, You
+ *   can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is the Team15 library.
- *
- * The Initial Developer of the Original Code is
- * Stéphan Kochen <stephan@kochen.nl>
- * Portions created by the Initial Developer are Copyright (C) 2006
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
+ * Authors:
+ *   Stéphan Kochen <stephan@kochen.nl>
  *   Vladimir Panteleev <vladimir@thecybershadow.net>
  *   Vincent Povirk <madewokherd@gmail.com>
  *   Simon Arlott
- *
- * Alternatively, the contents of this file may be used under the terms of the
- * GNU General Public License Version 3 (the "GPL") or later, in which case
- * the provisions of the GPL are applicable instead of those above. If you
- * wish to allow use of your version of this file only under the terms of the
- * GPL, and not to allow others to use your version of this file under the
- * terms of the MPL, indicate your decision by deleting the provisions above
- * and replace them with the notice and other provisions required by the GPL.
- * If you do not delete the provisions above, a recipient may use your version
- * of this file under the terms of either the MPL or the GPL.
- *
- * ***** END LICENSE BLOCK ***** */
+ */
 
-/// Common IRC code.
 module ae.net.irc.common;
 
 import core.time;
