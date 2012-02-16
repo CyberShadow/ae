fcd9b06defba4295375e031e38fed596b8933164
Vladimir Panteleev <vladimir@thecybershadow.net>
2012-02-16 22:39:32 +0200

    ae.utils.meta: Add FieldList

--- utils/meta.d
+++ utils/meta.d
@@ -27,3 +27,37 @@ template ValueTuple(T...)
 {
 	alias T ValueTuple;
 }
+
+/// Like std.typecons.Tuple, but a template mixin.
+/// Unlike std.typecons.Tuple, names may not be omitted - but repeating types may be.
+/// Example: FieldList!(ubyte, "r", "g", "b", ushort, "a");
+mixin template FieldList(Fields...)
+{
+	mixin(GenFieldList!(void, Fields));
+}
+
+private template GenFieldList(T, Fields...)
+{
+	static if (Fields.length == 0)
+		enum GenFieldList = "";
+	else
+	{
+		static if (is(typeof(Fields[0]) == string))
+			enum GenFieldList = T.stringof ~ " " ~ Fields[0] ~ ";\n" ~ GenFieldList!(T, Fields[1..$]);
+		else
+			enum GenFieldList = GenFieldList!(Fields[0], Fields[1..$]);
+	}
+}
+
+unittest
+{
+	struct S
+	{
+		mixin FieldList!(ubyte, "r", "g", "b", ushort, "a");
+	}
+	S s;
+	static assert(is(typeof(s.r) == ubyte));
+	static assert(is(typeof(s.g) == ubyte));
+	static assert(is(typeof(s.b) == ubyte));
+	static assert(is(typeof(s.a) == ushort));
+}
