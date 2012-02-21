a4aa8e509f88dfd9930ed496ed8653f99201684a
Vladimir Panteleev <vladimir@thecybershadow.net>
2012-02-21 18:19:59 +0200

    Add ae.sys.signals (untested)

---
+++ sys/signals.d
@@ -0,0 +1,51 @@
+/**
+ * POSIX signal handlers.
+ *
+ * License:
+ *   This Source Code Form is subject to the terms of
+ *   the Mozilla Public License, v. 2.0. If a copy of
+ *   the MPL was not distributed with this file, You
+ *   can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Authors:
+ *   Vladimir Panteleev <vladimir@thecybershadow.net>
+ */
+
+module ae.sys.signals;
+
+public import core.sys.posix.signal;
+
+void addSignalHandler(int signum, void delegate() fn)
+{
+	if (handlers[signum].length == 0)
+	{
+		auto old = signal(signum, &sighandle);
+		assert(old == SIG_DFL, "A signal handler was already set");
+	}
+	handlers[signum] ~= fn;
+}
+
+void removeSignalHandler(int signum, void delegate() fn)
+{
+	foreach (i, lfn; handlers[signum])
+		if (lfn is fn)
+		{
+			handlers[signum] = handlers[signum][0..i] ~ handlers[signum][i+1..$];
+			if (handlers[signum].length == 0)
+				signal(signum, SIG_DFL);
+			return;
+		}
+	assert(0);
+}
+
+private:
+
+enum SIGMAX = 100;
+shared void delegate()[][SIGMAX] handlers;
+
+extern(C) void sighandle(int signum)
+{
+	if (signum >= 0 && signum < handlers.length)
+		foreach (fn; handlers[signum])
+			fn();
+}
