3c6cedbf3eaa744a667bc0e505d66c41f417a1b8
Vladimir Panteleev <vladimir@thecybershadow.net>
2012-03-21 15:16:12 +0200

    ae.utils.meta_x: Add ddoc header

--- utils/meta_x.d
+++ utils/meta_x.d
@@ -1,3 +1,16 @@
+/**
+ * An implementation of Timon Gehr's X template
+ *
+ * License:
+ *   This Source Code Form is subject to the terms of
+ *   the Mozilla Public License, v. 2.0. If a copy of
+ *   the MPL was not distributed with this file, You
+ *   can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Authors:
+ *   Vladimir Panteleev <vladimir@thecybershadow.net>
+ */
+
 module ae.utils.meta_x;
 
 // Based on idea from Timon Gehr.
