CyberShadow/ae

commit 7121995320e5a07f9236686a233b09f601a72bb9

ae.sys.data: Fix Linux deprecation error

sys/data.d
@@ -520,7 +520,13 @@ final class DataWrapper
 		else
 		version(Posix)
 		{
-			auto p = mmap(null, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+			auto mapFlags = MAP_PRIVATE;
+			version(Linux)
+			{
+				import core.sys.linux.sys.mman;
+				mapFlags |= MAP_ANON;
+			}
+			auto p = mmap(null, size, PROT_READ | PROT_WRITE, mapFlags, -1, 0);
 			return (p == MAP_FAILED) ? null : p;
 		}
 		else

commit 33a14458f3c08bc776e70b7e3d506364f6d4de72

ae.sys.data: Fix OutofMemoryError regression on Linux
Regression introduced in commit 7121995320e5a07f9236686a233b09f601a72bb9

sys/data.d
@@ -521,7 +521,7 @@ final class DataWrapper
 		version(Posix)
 		{
 			auto mapFlags = MAP_PRIVATE;
-			version(Linux)
+			version(linux)
 			{
 				import core.sys.linux.sys.mman;
 				mapFlags |= MAP_ANON;