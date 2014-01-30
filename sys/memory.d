0f0d3288397e640b0b7b32e1e60107677bf98800
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-01-30 08:24:30 +0000

    ae.sys.memory: Add onStack

--- sys/memory.d
+++ sys/memory.d
@@ -44,3 +44,26 @@ bool gcRan()
 
 	return result;
 }
+
+import core.thread;
+
+/// Is the given pointer located on the stack of the current thread?
+/// Useful to assert on before taking the address of e.g. a struct member.
+bool onStack(const(void)* p)
+{
+	auto p0 = thread_stackTop();
+	auto p1 = thread_stackBottom();
+	return p0 <= p && p <= p1;
+}
+
+unittest
+{
+	/* .......... */ int l; auto pl = &l;
+	static /* ... */ int s; auto ps = &s;
+	static __gshared int g; auto pg = &g;
+	/* ................. */ auto ph = new int;
+	assert( pl.onStack());
+	assert(!ps.onStack());
+	assert(!pg.onStack());
+	assert(!ph.onStack());
+}
