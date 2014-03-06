1227e0c4ea93d81eeca41702af6f5e0af56d2ec3
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-03-06 20:54:07 +0000

    ae.utils.meta.binding: Add unboundFunctor, boundFunctor

--- utils/meta/binding.d
+++ utils/meta/binding.d
@@ -13,3 +13,98 @@
 
 module ae.utils.meta.binding;
 
+import ae.utils.meta.caps;
+import ae.utils.meta.misc : thisOf;
+import ae.utils.meta.reference;
+
+/// Create unbound functor of a method
+template unboundFunctorOf(alias f)
+{
+	static @property auto unboundFunctorOf()
+	{
+		UnboundFunctorOf!f r;
+		return r;
+	}
+}
+struct UnboundFunctorOf(alias f)
+{
+	alias opCall = f;
+
+	alias R = RefType!(thisOf!f);
+	auto bind(R r) { return boundFunctorOf!f(r); }
+}
+
+/// Create bound functor of a method
+template boundFunctorOf(alias f)
+{
+	static @property auto boundFunctorOf(T)(T context)
+	{
+		BoundFunctorOf!(T, f) r;
+		r.context = context;
+		return r;
+	}
+}
+
+/// ditto
+@property auto boundFunctorOf(alias f)()
+{
+	BoundFunctorOf!(RefType!(typeof(this)), f) r;
+	r.context = this.reference;
+	return r;
+}
+
+struct BoundFunctorOf(R, alias f)
+{
+	R context;
+	auto opCall(Args...)(auto ref Args args)
+	{
+		return __traits(child, context, f)(args);
+	}
+
+	/// Ignore - BoundFunctors are already bound
+	auto bind(R)(R r) { return this; }
+}
+
+static if (haveAliasCtxInference)
+unittest
+{
+	static struct Test
+	{
+		void caller(Func)(Func func)
+		{
+			func();
+		}
+
+		int i = 0;
+
+		void callee()
+		{
+			i++;
+		}
+
+		void test()
+		{
+			caller(unboundFunctorOf!callee);
+			caller(  boundFunctorOf!callee);
+
+			assert(i == 2);
+
+			static struct S
+			{
+				int i = 0;
+
+				void callee()
+				{
+					i++;
+				}
+			}
+			S s;
+			caller(boundFunctorOf!(S.callee)(&s));
+
+			assert(s.i == 1);
+		}
+	}
+
+	Test test;
+	test.test();
+}
