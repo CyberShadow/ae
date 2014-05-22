be84b50fd100f8cd9e68d7f9b46dfd93ea986378
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-05-22 03:55:55 +0000

    ae.sys.git: Add history querying
    Moved from D-dot-git

--- sys/git.d
+++ sys/git.d
@@ -13,6 +13,7 @@
 
 module ae.sys.git;
 
+import std.conv;
 import std.exception;
 import std.file;
 import std.path;
@@ -65,4 +66,102 @@ struct Repository
 				return null;
 		}
 	}
+
+	History getHistory()
+	{
+		History history;
+
+		Commit* getCommit(Hash hash)
+		{
+			auto pcommit = hash in history.commits;
+			return pcommit ? *pcommit : (history.commits[hash] = new Commit(history.numCommits++, hash));
+		}
+
+		Commit* commit;
+
+		foreach (line; query([`log`, `--all`, `--pretty=raw`]).splitLines())
+		{
+			if (!line.length)
+				continue;
+
+			if (line.startsWith("commit "))
+			{
+				auto hash = line[7..$].toCommitHash();
+				commit = getCommit(hash);
+			}
+			else
+			if (line.startsWith("tree "))
+				continue;
+			else
+			if (line.startsWith("parent "))
+			{
+				auto hash = line[7..$].toCommitHash();
+				auto parent = getCommit(hash);
+				commit.parents ~= parent;
+				parent.children ~= commit;
+			}
+			else
+			if (line.startsWith("author "))
+				commit.author = line[7..$];
+			else
+			if (line.startsWith("committer "))
+			{
+				commit.committer = line[10..$];
+				commit.time = line.split(" ")[$-2].to!int();
+			}
+			else
+			if (line.startsWith("    "))
+				commit.message ~= line[4..$];
+			else
+				//enforce(false, "Unknown line in git log: " ~ line);
+				commit.message[$-1] ~= line;
+		}
+
+		foreach (line; query([`show-ref`, `--dereference`]).splitLines())
+		{
+			auto h = line[0..40].toCommitHash();
+			if (h in history.commits)
+				history.refs[line[41..$]] = h;
+		}
+
+		return history;
+	}
+}
+
+static struct History
+{
+	Commit*[Hash] commits;
+	uint numCommits = 0;
+	Hash[string] refs;
+}
+
+alias ubyte[20] Hash;
+
+struct Commit
+{
+	uint id;
+	Hash hash;
+	uint time;
+	string author, committer;
+	string[] message;
+	Commit*[] parents, children;
+}
+
+Hash toCommitHash(string hash)
+{
+	enforce(hash.length == 40, "Bad hash length");
+	ubyte[20] result;
+	foreach (i, ref b; result)
+		b = to!ubyte(hash[i*2..i*2+2], 16);
+	return result;
+}
+
+string toString(ref Hash hash)
+{
+	return format("%(%02x%)", hash[]);
+}
+
+unittest
+{
+	assert(toCommitHash("0123456789abcdef0123456789ABCDEF01234567") == [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45, 0x67]);
 }
