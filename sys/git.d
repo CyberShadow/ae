18953fe417ed8b5f679313279d7362a5ab402220
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-07-12 23:21:17 +0300

    ae.sys.git: Add getObjects, writeObjects, createObjectWriter, etc.

--- sys/git.d
+++ sys/git.d
@@ -13,15 +13,22 @@
 
 module ae.sys.git;
 
+import std.algorithm;
+import std.array;
 import std.conv;
 import std.exception;
 import std.file;
+import std.format;
 import std.path;
 import std.process;
 import std.string;
+import std.typecons;
+import std.utf;
 
 import ae.sys.cmd;
 import ae.sys.file;
+import ae.utils.aa;
+import ae.utils.text;
 
 struct Repository
 {
@@ -52,6 +59,7 @@ struct Repository
 	void   run  (string[] args...) { auto owd = pushd(workPath(args[0])); return .run  (argsPrefix ~ args); }
 	string query(string[] args...) { auto owd = pushd(workPath(args[0])); return .query(argsPrefix ~ args); }
 	bool   check(string[] args...) { auto owd = pushd(workPath(args[0])); return spawnProcess(argsPrefix ~ args).wait() == 0; }
+	auto   pipe (string[] args...) { auto owd = pushd(workPath(args[0])); return pipeProcess(argsPrefix ~ args); }
 
 	/// Certain git commands (notably, bisect) must
 	/// be run in the repository's root directory.
@@ -126,9 +134,193 @@ struct Repository
 
 		return history;
 	}
+
+	/// Run a batch cat-file query.
+	GitObject[] getObjects(Hash[] hashes)
+	{
+		GitObject[] result;
+		result.reserve(hashes.length);
+
+		auto pipes = this.pipe(`cat-file`, `--batch`);
+		foreach (n, hash; hashes)
+		{
+			pipes.stdin.writeln(hash.toString());
+			pipes.stdin.flush();
+
+			auto headerLine = pipes.stdout.readln().strip();
+			auto header = headerLine.split(" ");
+			enforce(header.length == 3, "Malformed header during cat-file: " ~ headerLine);
+			enforce(header[0].toCommitHash() == hash, "Unexpected object during cat-file");
+
+			GitObject obj;
+			obj.hash = hash;
+			obj.type = header[1];
+			auto size = to!size_t(header[2]);
+			auto data = new ubyte[size];
+			auto read = pipes.stdout.rawRead(data);
+			enforce(read.length == size, "Unexpected EOF during cat-file");
+			obj.data = data.assumeUnique();
+
+			char[1] lf;
+			pipes.stdout.rawRead(lf[]);
+			enforce(lf[0] == '\n', "Terminating newline expected");
+
+			result ~= obj;
+		}
+		pipes.stdin.close();
+		enforce(pipes.pid.wait() == 0, "git cat-file exited with failure");
+		return result;
+	}
+
+	struct ObjectWriterImpl
+	{
+		ProcessPipes pipes;
+
+		Hash write(in void[] data)
+		{
+			string fileName;
+			auto f = createNamedPipe("ae-sys-git-writeObjects", fileName);
+			pipes.stdin.writeln(fileName);
+			pipes.stdin.flush();
+
+			connectNamedPipe(f);
+			f.rawWrite(data);
+			f.flush();
+			f.close();
+
+			return pipes.stdout.readln().strip().toCommitHash();
+		}
+
+		~this()
+		{
+			pipes.stdin.close();
+			enforce(pipes.pid.wait() == 0, "git hash-object exited with failure");
+		}
+	}
+	alias ObjectWriter = RefCounted!ObjectWriterImpl;
+
+	/// Spawn a hash-object process which can hash and write git objects on the fly.
+	ObjectWriter createObjectWriter(string type)
+	{
+		auto pipes = this.pipe(`hash-object`, `-t`, type, `-w`, `--stdin-paths`);
+		return ObjectWriter(pipes);
+	}
+
+	/// Batch-write the given objects to the database.
+	/// The hashes are saved to the "hash" fields of the passed objects.
+	void writeObjects(GitObject[] objects)
+	{
+		string[] allTypes = objects.map!(obj => obj.type).toSet().keys;
+		foreach (type; allTypes)
+		{
+			auto writer = createObjectWriter(type);
+			foreach (ref obj; objects)
+				if (obj.type == type)
+					obj.hash = writer.write(obj.data);
+		}
+	}
 }
 
-static struct History
+struct GitObject
+{
+	Hash hash;
+	string type;
+	immutable(ubyte)[] data;
+
+	struct ParsedCommit
+	{
+		Hash tree;
+		Hash[] parents;
+		string author, committer; /// entire lines - name, email and date
+		string[] message;
+	}
+
+	ParsedCommit parseCommit()
+	{
+		enforce(type == "commit", "Wrong object type");
+		ParsedCommit result;
+		auto lines = (cast(string)data).split('\n');
+		foreach (n, line; lines)
+		{
+			if (line == "")
+			{
+				result.message = lines[n+1..$];
+				break; // commit message begins
+			}
+			auto parts = line.findSplit(" ");
+			auto field = parts[0];
+			line = parts[2];
+			switch (field)
+			{
+				case "tree":
+					result.tree = line.toCommitHash();
+					break;
+				case "parent":
+					result.parents ~= line.toCommitHash();
+					break;
+				case "author":
+					result.author = line;
+					break;
+				case "committer":
+					result.committer = line;
+					break;
+				default:
+					throw new Exception("Unknown commit field: " ~ field);
+			}
+		}
+		return result;
+	}
+
+	static GitObject createCommit(ParsedCommit commit)
+	{
+		auto s = "tree %s\n%-(parent %s\n%|%)author %s\ncommitter %s\n\n%-(%s\n%)".format(
+				commit.tree.toString(),
+				commit.parents.map!(ae.sys.git.toString),
+				commit.author,
+				commit.committer,
+				commit.message,
+			);
+		return GitObject(Hash.init, "commit", cast(immutable(ubyte)[])s);
+	}
+
+	struct TreeEntry
+	{
+		uint mode;
+		string name;
+		Hash hash;
+	}
+
+	TreeEntry[] parseTree()
+	{
+		enforce(type == "tree", "Wrong object type");
+		TreeEntry[] result;
+		auto rem = data;
+		while (rem.length)
+		{
+			auto si = rem.countUntil(' ');
+			auto zi = rem.countUntil(0);
+			auto ei = zi + 1 + Hash.sizeof;
+			auto str = cast(string)rem[0..zi];
+			enforce(0 < si && si < zi && ei <= rem.length, "Malformed tree entry:\n" ~ hexDump(rem));
+			result ~= TreeEntry(str[0..si].to!uint(8), str[si+1..zi], cast(Hash)rem[zi+1..ei]); // https://issues.dlang.org/show_bug.cgi?id=13112
+			rem = rem[ei..$];
+		}
+		return result;
+	}
+
+	static GitObject createTree(TreeEntry[] entries)
+	{
+		auto buf = appender!(ubyte[]);
+		foreach (entry; entries)
+		{
+			buf.formattedWrite("%o %s\0", entry.mode, entry.name);
+			buf.put(entry.hash[]);
+		}
+		return GitObject(Hash.init, "tree", buf.data.assumeUnique);
+	}
+}
+
+struct History
 {
 	Commit*[Hash] commits;
 	uint numCommits = 0;
@@ -147,9 +339,9 @@ struct Commit
 	Commit*[] parents, children;
 }
 
-Hash toCommitHash(string hash)
+Hash toCommitHash(in char[] hash)
 {
-	enforce(hash.length == 40, "Bad hash length");
+	enforce(hash.length == 40, "Bad hash length: " ~ hash);
 	ubyte[20] result;
 	foreach (i, ref b; result)
 		b = to!ubyte(hash[i*2..i*2+2], 16);
