1840005ae504796bfeaceff30df3517f544cb3b7
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-04-03 11:37:51 +0000

    ae.utils.graphics.im_convert: Force conversion to RGB pixel format

--- utils/graphics/im_convert.d
+++ utils/graphics/im_convert.d
@@ -15,12 +15,21 @@ module ae.utils.graphics.im_convert;
 
 import ae.sys.cmd;
 import ae.sys.imagemagick;
+import ae.utils.graphics.color;
 import ae.utils.graphics.image;
 
 auto parseViaIMConvert(C = TargetColor, TARGET)(const(void)[] data, auto ref TARGET target)
 	if (isWritableView!TARGET && isTargetColor!(C, TARGET))
 {
-	return pipe(["convert".imageMagickBinary(), "-", "bmp:-"], data).parseBMP(target);
+	string[] convertFlags;
+	static if (is(ViewColor!TARGET : BGR))
+	{
+	//	convertFlags ~= ["-colorspace", "rgb"];
+	//	convertFlags ~= ["-depth", "24"];
+		convertFlags ~= ["-type", "TrueColor"];
+		convertFlags ~= ["-alpha", "off"];
+	}
+	return pipe(["convert".imageMagickBinary()] ~ convertFlags ~ ["-[0]", "bmp:-"], data).parseBMP(target);
 }
 
 auto parseViaIMConvert(COLOR)(const(void)[] data)
