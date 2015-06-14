c0c0ec60de6c3ce474285e189503dddca8edbc16
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-06-14 23:00:46 +0000

    ae.net.http.responseex: Add ResponseEx.dup

--- net/http/responseex.d
+++ net/http/responseex.d
@@ -253,6 +253,16 @@ public:
 		.cacheForever(headers);
 	}
 
+	HttpResponseEx dup()
+	{
+		auto c = new HttpResponseEx;
+		c.status = this.status;
+		c.statusMessage = this.statusMessage;
+		c.headers = this.headers.dup;
+		c.data = this.data.dup;
+		return c;
+	}
+
 	static pageTemplate =
 `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
