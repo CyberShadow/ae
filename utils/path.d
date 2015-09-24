3a0f3c6e062f79a8aa656c4d2e0165b07bb26867
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-09-24 08:18:17 +0000

    ae.utils.path: Initial commit

---
+++ utils/path.d
@@ -0,0 +1,21 @@
+﻿/**
+ * ae.utils.path
+ *
+ * License:
+ *   This Source Code Form is subject to the terms of
+ *   the Mozilla Public License, v. 2.0. If a copy of
+ *   the MPL was not distributed with this file, You
+ *   can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Authors:
+ *   Vladimir Panteleev <vladimir@thecybershadow.net>
+ */
+
+module ae.utils.path;
+
+import std.path;
+
+string rebasePath(string path, string oldBase, string newBase)
+{
+	return buildPath(newBase, path.absolutePath.relativePath(oldBase.absolutePath));
+}
