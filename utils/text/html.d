dccffc2f0dff2688a170a8cfd8939a55725f4c0a
Vladimir Panteleev <git@thecybershadow.net>
2016-04-30 15:42:13 +0000

    ae.utils.text.html: Initial commit

---
+++ utils/text/html.d
@@ -0,0 +1,45 @@
+﻿/**
+ * ae.utils.text.html
+ *
+ * License:
+ *   This Source Code Form is subject to the terms of
+ *   the Mozilla Public License, v. 2.0. If a copy of
+ *   the MPL was not distributed with this file, You
+ *   can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Authors:
+ *   Vladimir Panteleev <vladimir@thecybershadow.net>
+ */
+
+module ae.utils.text.html;
+
+import ae.utils.textout;
+
+string encodeHtmlEntities(bool inAttribute = true)(string s)
+{
+	StringBuilder result;
+	size_t start = 0;
+
+	foreach (i, c; s)
+		if (c=='<')
+			result.put(s[start..i], "&lt;"),
+			start = i+1;
+		else
+		if (c=='>')
+			result.put(s[start..i], "&gt;"),
+			start = i+1;
+		else
+		if (c=='&')
+			result.put(s[start..i], "&amp;"),
+			start = i+1;
+		else
+		if (inAttribute && c=='"')
+			result.put(s[start..i], "&quot;"),
+			start = i+1;
+
+	if (!start)
+		return s;
+
+	result.put(s[start..$]);
+	return result.get();
+}
