CyberShadow/ae

commit b009d96ec93c8b5d5818c2d169e94c8e2082ecb0

ae.sys.net: Add post() primitive

sys/net/wininet.d
@@ -84,9 +84,9 @@ protected:
 		return HNet(hReq);
 	}
 
-	final static void sendRequest(ref HNet hReq)
+	final static void sendRequest(ref HNet hReq, string headers = null, in void[] optionalData = null)
 	{
-		HttpSendRequestA(hReq, null, 0, null, 0);
+		HttpSendRequestA(hReq, headers.ptr, headers.length, cast(void*)optionalData.ptr, optionalData.length);
 			.wenforce("HttpSendRequest");
 	}
 
@@ -136,12 +136,8 @@ protected:
 		return results[0];
 	}
 
-	final static void doDownload(string url, void delegate(ubyte[]) sink)
+	final static void doDownload(HNet hUrl, void delegate(ubyte[]) sink)
 	{
-		auto hNet = open();
-		auto hUrl = hNet
-			.I!openUrl(url);
-
 		// Check HTTP status code
 		auto statusCode = hUrl.I!httpQueryNumber(HTTP_QUERY_STATUS_CODE);
 		if (statusCode != 200)
@@ -182,23 +178,29 @@ public:
 	{
 		import std.stdio;
 		auto f = File(target, "wb");
-		doDownload(url,
-			(ubyte[] bytes)
-			{
-				f.rawWrite(bytes);
-			}
-		);
+		auto hNet = open();
+		doDownload(hNet.I!openUrl(url), &f.rawWrite!ubyte);
 	}
 
 	override void[] getFile(string url)
 	{
 		auto result = appender!(ubyte[]);
-		doDownload(url,
-			(ubyte[] bytes)
-			{
-				result.put(bytes);
-			}
-		);
+		auto hNet = open();
+		doDownload(hNet.I!openUrl(url), &result.put!(ubyte[]));
+		return result.data;
+	}
+
+	override void[] post(string url, in void[] data)
+	{
+		auto request = new HttpRequest(url);
+
+		auto hNet = open();
+		auto hCon = hNet.I!connect(request.host, request.port);
+		auto hReq = hCon.I!openRequest("POST", request.resource, urlFlags(url));
+		hReq.I!sendRequest(null, data);
+
+		auto result = appender!(ubyte[]);
+		doDownload(hReq, &result.put!(ubyte[]));
 		return result.data;
 	}
 

sys/net/ae.d
@@ -49,6 +49,21 @@ class AENetwork : Network
 		return getData(url).toHeap;
 	}
 
+	override void[] post(string url, in void[] data)
+	{
+		Data result;
+		bool got;
+
+		httpPost(url, [Data(data)], null,
+			(Data data) { result = data; got = true; },
+			(string error) { throw new Exception(error); }
+		);
+
+		socketManager.loop();
+		assert(got);
+		return result.toHeap;
+	}
+
 	override bool urlOK(string url)
 	{
 		bool got, result;

sys/net/cachedcurl.d
@@ -169,8 +169,7 @@ class CachedCurlNetwork : Network
 			[$-1];
 	}
 
-	// TODO: convert to Network primitive
-	ubyte[] cachedPost(string url, in void[] data)
+	override void[] post(string url, in void[] data)
 	{
 		return cachedReq(url, HTTP.Method.post, data).responseData;
 	}

sys/net/curl.d
@@ -33,6 +33,11 @@ class CurlNetwork : Network
 		return get!(AutoProtocol, ubyte)(url);
 	}
 
+	override void[] post(string url, in void[] data)
+	{
+		return .post!ubyte(url, data);
+	}
+
 	override bool urlOK(string url)
 	{
 		try

sys/net/package.d
@@ -36,6 +36,14 @@ class Network
 		assert(false);
 	}
 
+	/// Post data to the specified URL.
+	// TODO: Content-Type?
+	void[] post(string url, in void[] data)
+	{
+		notImplemented();
+		assert(false);
+	}
+
 	/// Check if the resource exists and is downloadable.
 	/// E.g. the HTTP status code for a HEAD request should be 200.
 	bool urlOK(string url)
@@ -69,5 +77,6 @@ static this()
 /// UFCS-able global synonym functions.
 void downloadFile(string url, string target) { net.downloadFile(url, target); }
 void[] getFile(string url) { return net.getFile(url); } /// ditto
+void[] post(string url, void[] data) { return net.post(url, data); }
 bool urlOK(string url) { return net.urlOK(url); } /// ditto
 string resolveRedirect(string url) { return net.resolveRedirect(url); } /// ditto

sys/net/test.d
@@ -61,6 +61,11 @@ void test(string moduleName, string className)()
 		auto result = net.resolveRedirect("http://thecybershadow.net/d/nettest/testUrl3");
 		assert(result == "http://thecybershadow.net/d/nettest/testUrl2", result);
 	}
+
+	debug std.stdio.stderr.writeln(" - post");
+	{
+		assert(net.post("http://thecybershadow.net/d/nettest/testUrl4", "Hello world\n") == "Hello world\n");
+	}
 }
 
 unittest

commit deaa0b522ae6500a2f992f96146a6b051d76ccf0

ae.sys.net.wininet: Fix compilation on DMD < v2.075
Broken in b009d96ec93c8b5d5818c2d169e94c8e2082ecb0.

sys/net/wininet.d
@@ -139,6 +139,11 @@ protected:
 
 	final static void doDownload(HNet hUrl, void delegate(ubyte[]) sink)
 	{
+		doDownload(hUrl, cast(void delegate(const ubyte[])) sink);
+	}
+
+	final static void doDownload(HNet hUrl, void delegate(const ubyte[]) sink)
+	{
 		// Check HTTP status code
 		auto statusCode = hUrl.I!httpQueryNumber(HTTP_QUERY_STATUS_CODE);
 		if (statusCode != 200)