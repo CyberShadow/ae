CyberShadow/ae

commit 11c51c1cbc826d264b7c01b174b914d72a40a4bb

ae.sys.net.wininet: Fix compilation on Win64

sys/net/wininet.d
@@ -86,7 +86,7 @@ protected:
 
 	final static void sendRequest(ref HNet hReq, string headers = null, in void[] optionalData = null)
 	{
-		HttpSendRequestA(hReq, headers.ptr, headers.length, cast(void*)optionalData.ptr, optionalData.length);
+		HttpSendRequestA(hReq, headers.ptr, headers.length.to!DWORD, cast(void*)optionalData.ptr, optionalData.length.to!DWORD);
 			.wenforce("HttpSendRequest");
 	}
 

commit f7ec600ad08cac5b92a9cffa9af0965f9296aad2

ae.sys.net.wininet: Add missing import
Broken in 11c51c1cbc826d264b7c01b174b914d72a40a4bb.

sys/net/wininet.d
@@ -16,6 +16,7 @@ module ae.sys.net.wininet;
 version(Windows):
 
 import std.array;
+import std.conv : to;
 import std.exception;
 import std.string;
 import std.typecons : RefCounted;