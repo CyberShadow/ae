c649fd37c16114bbb8d30c0cafb2b1e7c57f93ae
Vladimir Panteleev <git@thecybershadow.net>
2018-01-25 16:09:05 +0000

    ae.ui.video.bmfont: Initial commit

---
+++ ui/video/bmfont.d
@@ -0,0 +1,71 @@
+/**
+ * Rendering for simple bitmap fonts.
+ *
+ * License:
+ *   This Source Code Form is subject to the terms of
+ *   the Mozilla Public License, v. 2.0. If a copy of
+ *   the MPL was not distributed with this file, You
+ *   can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Authors:
+ *   Vladimir Panteleev <vladimir@thecybershadow.net>
+ */
+
+module ae.ui.video.bmfont;
+
+import ae.ui.video.renderer;
+import ae.utils.array;
+import ae.utils.graphics.fonts.draw;
+
+final class FontTextureSource(Font) : ProceduralTextureSource
+{
+	Font font;
+	Renderer.COLOR color;
+
+	this(Font font, Renderer.COLOR color)
+	{
+		this.font = font;
+		this.color = color;
+	}
+
+	override void getSize(out int width, out int height)
+	{
+		width = font.maxWidth;
+		height = font.maxGlyph * font.height;
+	}
+
+	override void drawTo(TextureCanvas dest)
+	{
+		foreach (g; 0..font.maxGlyph)
+		{
+			dchar c = g;
+			dest.drawText(0, g * font.height, c.toArray, font, color);
+		}
+	}
+
+	void drawText(S)(Renderer r, int x, int y, S s)
+	{
+		foreach (c; s)
+		{
+			if (font.hasGlyph(c))
+			{
+				auto g = font.getGlyph(c);
+				auto v = c * font.height;
+				r.draw(x, y, this, 0, v, g.width, v + font.height);
+				x += g.width;
+			}
+		}
+	}
+}
+
+unittest
+{
+	// Test instantiation
+	if (false)
+	{
+		Renderer r;
+		import ae.utils.graphics.fonts.font8x8;
+		FontTextureSource!Font8x8 f;
+		f.drawText(r, 0, 0, "foo");
+	}
+}
