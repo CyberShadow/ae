db0b16c4ba747b5935fb405c9ca2bdffaa42b4d9
Vladimir Panteleev <git@thecybershadow.net>
2018-05-16 15:42:58 +0000

    ae.utils.sound.wave: Use infiniteIota

--- utils/sound/wave.d
+++ utils/sound/wave.d
@@ -18,33 +18,35 @@ import std.conv;
 import std.math;
 import std.range;
 
+import ae.utils.range;
+
 auto squareWave(T)(real interval)
 {
-	return iota(size_t.max)
+	return infiniteIota!size_t
 		.map!(n => cast(T)(T.max + cast(int)(n * 2 / interval) % 2));
 }
 
 auto sawToothWave(T)(real interval)
 {
-	return iota(size_t.max)
+	return infiniteIota!size_t
 		.map!(n => cast(T)((n % interval * 2 - interval) * T.max / interval));
 }
 
 auto triangleWave(T)(real interval)
 {
-	return iota(size_t.max)
+	return infiniteIota!size_t
 		.map!(n => cast(T)((abs(n % interval * 2 - interval) * 2 - interval) * T.max / interval));
 }
 
 auto sineWave(T)(real interval)
 {
-	return iota(size_t.max)
+	return infiniteIota!size_t
 		.map!(n => (sin(n * 2 * PI / interval) * T.max).to!T);
 }
 
 auto whiteNoise(T)()
 {
 	import std.random;
-	return iota(size_t.max)
+	return infiniteIota!size_t
 		.map!(n => cast(T)Xorshift(cast(uint)n).front);
 }
