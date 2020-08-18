466107e3c85934b718093cd955e26280e4f9dc5e
Vladimir Panteleev <git@thecybershadow.net>
2020-08-18 08:28:36 +0000

    ae.net.ssl.test: Switch to using badssl.com for thorough testing

--- net/ssl/test.d
+++ net/ssl/test.d
@@ -13,26 +13,35 @@
 
 module ae.net.ssl.test;
 
+import std.exception;
+
 import ae.net.asockets;
 import ae.net.ssl;
 
-debug(SSL) import std.stdio : stderr;
+version (SSL_test)
+	void testSSL(SSLProvider ssl) {}
+else:
+
+import std.stdio : stderr;
 
 void testSSL(SSLProvider ssl)
 {
-	import std.algorithm.searching : endsWith;
+	import std.algorithm.searching : startsWith;
+	debug(SSL) import std.stdio : stderr;
 
-	void testServer(string host, ushort port)
+	void testServer(string host, ushort port = 443)
 	{
+		stderr.writeln("Testing: ", host);
 		auto c = new TcpConnection;
 		auto ctx = ssl.createContext(SSLContext.Kind.client);
+		ctx.setPeerVerify(SSLContext.Verify.require);
 		auto s = ssl.createAdapter(ctx, c);
 		Data allData;
 
 		s.handleConnect =
 		{
 			debug(SSL) stderr.writeln("Connected!");
-			s.send(Data("GET /d/nettest/testUrl1 HTTP/1.0\r\nHost: thecybershadow.net\r\n\r\n"));
+			s.send(Data("GET / HTTP/1.0\r\nHost: " ~ host ~ "\r\n\r\n"));
 		};
 		s.handleReadData = (Data data)
 		{
@@ -42,13 +51,62 @@ void testSSL(SSLProvider ssl)
 		s.handleDisconnect = (string reason, DisconnectType type)
 		{
 			debug(SSL) { stderr.writeln(reason); }
-			assert(type == DisconnectType.graceful);
-			assert((cast(string)allData.contents).endsWith("Hello world\n"));
+			enforce(type == DisconnectType.graceful, "Unexpected disconnection: " ~ reason);
+			enforce((cast(string)allData.contents).startsWith("HTTP/1.1 200 OK\r\n"), "Unexpected response");
 		};
-		s.setHostName("thecybershadow.net");
+		s.setHostName(host);
 		c.connect(host, port);
 		socketManager.loop();
 	}
 
-	testServer("thecybershadow.net", 443);
+	testServer(              "expired.badssl.com").assertThrown;
+	testServer(           "wrong.host.badssl.com").assertThrown;
+	testServer(          "self-signed.badssl.com").assertThrown;
+	testServer(       "untrusted-root.badssl.com").assertThrown;
+	// testServer(              "revoked.badssl.com").assertThrown; // TODO!
+	// testServer(         "pinning-test.badssl.com").assertThrown; // TODO!
+
+	// testServer(       "no-common-name.badssl.com").assertNotThrown; // Currently expired
+	// testServer(           "no-subject.badssl.com").assertNotThrown; // Currently expired
+	// testServer(     "incomplete-chain.badssl.com").assertNotThrown; // TODO?
+
+	testServer(               "sha256.badssl.com").assertNotThrown;
+	testServer(               "sha384.badssl.com").assertNotThrown;
+	testServer(               "sha512.badssl.com").assertNotThrown;
+
+	testServer(            "1000-sans.badssl.com").assertNotThrown;
+	testServer(           "10000-sans.badssl.com").assertThrown;
+
+	testServer(               "ecc256.badssl.com").assertNotThrown;
+	testServer(               "ecc384.badssl.com").assertNotThrown;
+
+	testServer(              "rsa2048.badssl.com").assertNotThrown;
+	testServer(              "rsa4096.badssl.com").assertNotThrown;
+	testServer(              "rsa8192.badssl.com").assertNotThrown;
+
+	testServer(  "extended-validation.badssl.com").assertNotThrown;
+
+	testServer(                  "cbc.badssl.com").assertNotThrown;
+	testServer(              "rc4-md5.badssl.com").assertThrown;
+	testServer(                  "rc4.badssl.com").assertThrown;
+	testServer(                 "3des.badssl.com").assertThrown;
+	testServer(                 "null.badssl.com").assertThrown;
+
+	testServer(          "mozilla-old.badssl.com").assertNotThrown; // Watch me, browsers may drop support soon
+	testServer( "mozilla-intermediate.badssl.com").assertNotThrown;
+	testServer(       "mozilla-modern.badssl.com").assertNotThrown;
+
+	testServer(                "dh480.badssl.com").assertThrown;
+	testServer(                "dh512.badssl.com").assertThrown;
+	testServer(               "dh1024.badssl.com").assertThrown;
+	testServer(               "dh2048.badssl.com").assertThrown;
+
+	testServer(    "dh-small-subgroup.badssl.com").assertThrown;
+	testServer(         "dh-composite.badssl.com").assertThrown;
+
+	testServer(           "static-rsa.badssl.com").assertNotThrown;
+
+	testServer(             "tls-v1-0.badssl.com", 1010).assertThrown;
+	testServer(             "tls-v1-1.badssl.com", 1011).assertThrown;
+	testServer(             "tls-v1-2.badssl.com", 1012).assertNotThrown;
 }
