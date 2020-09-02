554f6d5dec50dcfc2dda9e1c0d78d0f6fa554546
Vladimir Panteleev <git@thecybershadow.net>
2020-09-02 11:15:38 +0000

    ae.sys.install.kindlegen: Add urlDigests

--- sys/install/kindlegen.d
+++ sys/install/kindlegen.d
@@ -34,6 +34,13 @@ class KindleGenInstaller : Installer
 
 	string url = defaultUrl;
 
+	static this()
+	{
+		urlDigests["https://dump.cy.md/d4be194f848da73ea09742bc3a787f1b/kindlegen_win32_v2_9.zip"            ] = "8b5540f12e291b4031ad5197e11c0c9f576ad1e5";
+		urlDigests["https://dump.cy.md/21aef3c8846946203e178c83a37beba1/kindlegen_linux_2.6_i386_v2_9.tar.gz"] = "2e46cce099aba2725b5ba9ac9d1b1ecdc9dd77e2";
+		urlDigests["https://dump.cy.md/204a2a4cc3e95e1a0dbbb9e52a7bc482/KindleGen_Mac_i386_v2_9.zip"         ] = "efa5ad0b05650f7f71543535ea2b232bb5fec571";
+	}
+
 	@property override string[] requiredExecutables() { return ["kindlegen"]; }
 
 	override void installImpl(string target)
