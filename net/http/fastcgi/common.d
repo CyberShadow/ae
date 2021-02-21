bf685fd436d374f2b94c8e68459880b7124b2199
Vladimir Panteleev <git@thecybershadow.net>
2021-02-21 07:39:30 +0000

    ae.net.http.fastcgi.common: Document public symbols

--- net/http/fastcgi/common.d
+++ net/http/fastcgi/common.d
@@ -18,14 +18,15 @@ import ae.utils.bitmanip;
 /// Listening socket file number
 enum FCGI_LISTENSOCK_FILENO = /*STDIN_FILENO*/ 0;
 
+/// FastCGI protocol packet header.
 struct FCGI_RecordHeader
 {
-	ubyte version_;
-	FCGI_RecordType type;
-	BigEndian!ushort requestId;
-	BigEndian!ushort contentLength;
-	ubyte paddingLength;
-	ubyte reserved;
+	ubyte version_; /// FastCGI protocol version. Should be FCGI_VERSION_1.
+	FCGI_RecordType type; /// FastCGI record (packet) type.
+	BigEndian!ushort requestId; /// Request ID.
+	BigEndian!ushort contentLength; /// Number of bytes of data following this header.
+	ubyte paddingLength; /// Number of additional bytes used for padding.
+	ubyte reserved; /// Unused.
 }
 static assert(FCGI_RecordHeader.sizeof == 8);
 
@@ -35,28 +36,28 @@ enum FCGI_VERSION_1 = 1;
 /// Values for type component of FCGI_Header
 enum FCGI_RecordType : ubyte
 {
-	//                                                   WS->App   management  stream
-	beginRequest    =  1,   /// FCGI_BEGIN_REQUEST          x
-	abortRequest    =  2,   /// FCGI_ABORT_REQUEST          x
+	beginRequest    =  1,   /// FCGI_BEGIN_REQUEST     ( webserver-sent                     )
+	abortRequest    =  2,   /// FCGI_ABORT_REQUEST     ( webserver-sent                     )
 	endRequest      =  3,   /// FCGI_END_REQUEST
-	params          =  4,   /// FCGI_PARAMS                 x                    x
-	stdin           =  5,   /// FCGI_STDIN                  x                    x
-	stdout          =  6,   /// FCGI_STDOUT                                      x
-	stderr          =  7,   /// FCGI_STDERR                                      x
-	data            =  8,   /// FCGI_DATA                   x                    x
-	getValues       =  9,   /// FCGI_GET_VALUES             x          x
-	getValuesResult = 10,   /// FCGI_GET_VALUES_RESULT                 x
-	unknownType     = 11,   /// FCGI_UNKNOWN_TYPE                      x
+	params          =  4,   /// FCGI_PARAMS            ( webserver-sent,             stream )
+	stdin           =  5,   /// FCGI_STDIN             ( webserver-sent,             stream )
+	stdout          =  6,   /// FCGI_STDOUT            (                             stream )
+	stderr          =  7,   /// FCGI_STDERR            (                             stream )
+	data            =  8,   /// FCGI_DATA              ( webserver-sent,             stream )
+	getValues       =  9,   /// FCGI_GET_VALUES        ( webserver-sent, management         )
+	getValuesResult = 10,   /// FCGI_GET_VALUES_RESULT (                 management         )
+	unknownType     = 11,   /// FCGI_UNKNOWN_TYPE      (                 management         )
 }
 
-/// Value for requestId component of FCGI_Header
+/// Value for requestId component of FCGI_Header.
 enum FCGI_NULL_REQUEST_ID = 0;
 
+/// Structure of FCGI_BEGIN_REQUEST packets.
 struct FCGI_BeginRequestBody
 {
-	BigEndian!FCGI_Role role;
-    FCGI_RequestFlags flags;
-    ubyte[5] reserved;
+	BigEndian!FCGI_Role role; /// FastCGI application role.
+    FCGI_RequestFlags flags; /// Request flags.
+    ubyte[5] reserved; /// Unused.
 }
 
 /// Mask for flags component of FCGI_BeginRequestBody
@@ -73,11 +74,12 @@ enum FCGI_Role : ushort
 	filter     = 3, /// FCGI_FILTER
 }
 
+/// Structure of FCGI_END_REQUEST packets.
 struct FCGI_EndRequestBody
 {
-	BigEndian!uint appStatus;
-    FCGI_ProtocolStatus protocolStatus;
-    ubyte[3] reserved;
+	BigEndian!uint appStatus; /// Application-supplied status code.
+    FCGI_ProtocolStatus protocolStatus; /// FastCGI status code.
+    ubyte[3] reserved; /// Unused.
 }
 
 /// Values for protocolStatus component of FCGI_EndRequestBody
@@ -91,11 +93,12 @@ enum FCGI_ProtocolStatus : ubyte
 
 /// Variable names for FCGI_GET_VALUES / FCGI_GET_VALUES_RESULT records
 enum FCGI_MAX_CONNS  = "FCGI_MAX_CONNS";
-enum FCGI_MAX_REQS   = "FCGI_MAX_REQS";
-enum FCGI_MPXS_CONNS = "FCGI_MPXS_CONNS";
+enum FCGI_MAX_REQS   = "FCGI_MAX_REQS"; /// ditto
+enum FCGI_MPXS_CONNS = "FCGI_MPXS_CONNS"; /// ditto
 
+/// Structure of FCGI_UNKNOWN_TYPE packets.
 struct FCGI_UnknownTypeBody
 {
-    FCGI_RecordType type;
-    ubyte[7] reserved;
+    FCGI_RecordType type; /// The record type that was not recognized.
+    ubyte[7] reserved; /// Unused.
 }
