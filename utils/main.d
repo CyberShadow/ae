71e47bfd49a6f371e4289939d5611ccafbaff612
Vladimir Panteleev <git@cy.md>
2021-12-19 12:35:38 +0000

    ae.utils.main: Hide helper functions

--- utils/main.d
+++ utils/main.d
@@ -25,46 +25,46 @@ module ae.utils.main;
  */
 mixin template main(alias realMain)
 {
-	int run(string[] args)
-	{
-		static if (is(typeof(realMain())))
-			static if (is(typeof(realMain()) == void))
-				{ realMain(); return 0; }
-			else
-				return realMain();
-		else
-			static if (is(typeof(realMain(args)) == void))
-				{ realMain(args); return 0; }
-			else
-				return realMain(args);
-	}
-
-	int runCatchingException(E, string message)(string[] args)
-	{
-		try
-			return run(args);
-		catch (E e)
-		{
-			version (Windows)
-			{
-				import core.sys.windows.windows;
-				auto h = GetStdHandle(STD_ERROR_HANDLE);
-				if (!h || h == INVALID_HANDLE_VALUE)
-				{
-					import ae.sys.windows : messageBox;
-					messageBox(e.msg, message, MB_ICONERROR);
-					return 1;
-				}
-			}
-
-			import std.stdio : stderr;
-			stderr.writefln("%s: %s", message, e.msg);
-			return 1;
-		}
-	}
-
 	int main(string[] args)
 	{
+		int run(string[] args)
+		{
+			static if (is(typeof(realMain())))
+				static if (is(typeof(realMain()) == void))
+					{ realMain(); return 0; }
+				else
+					return realMain();
+			else
+				static if (is(typeof(realMain(args)) == void))
+					{ realMain(args); return 0; }
+				else
+					return realMain(args);
+		}
+
+		int runCatchingException(E, string message)(string[] args)
+		{
+			try
+				return run(args);
+			catch (E e)
+			{
+				version (Windows)
+				{
+					import core.sys.windows.windows;
+					auto h = GetStdHandle(STD_ERROR_HANDLE);
+					if (!h || h == INVALID_HANDLE_VALUE)
+					{
+						import ae.sys.windows : messageBox;
+						messageBox(e.msg, message, MB_ICONERROR);
+						return 1;
+					}
+				}
+
+				import std.stdio : stderr;
+				stderr.writefln("%s: %s", message, e.msg);
+				return 1;
+			}
+		}
+
 		debug
 			static if(is(std.getopt.GetOptException))
 				return runCatchingException!(std.getopt.GetOptException, "Usage error")(args);
