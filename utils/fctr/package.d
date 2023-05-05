6397a9cbd3a8db85dc320e26977cb8335c7ba66c
Vladimir Panteleev <git@cy.md>
2023-05-05 10:08:37 +0000

    ae.utils.fctr.package: Initial commit

---
+++ utils/fctr/package.d
@@ -0,0 +1,18 @@
+/**
+ * Functor package.
+ *
+ * License:
+ *   This Source Code Form is subject to the terms of
+ *   the Mozilla Public License, v. 2.0. If a copy of
+ *   the MPL was not distributed with this file, You
+ *   can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Authors:
+ *   Vladimir Panteleev <ae@cy.md>
+ */
+
+module ae.utils.fctr;
+
+public import ae.utils.fctr.algorithm;
+public import ae.utils.fctr.composition;
+public import ae.utils.fctr.primitives;
